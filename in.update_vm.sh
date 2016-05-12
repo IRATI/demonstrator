8e0f73fb9b34e911d6abefe553cc2e3181905142
Vincenzo Maffione <v.maffione@gmail.com>
2016-05-12 17:52:34 +0200

    update_vm.sh is not generated anymore

--- in.update_vm.sh
+++
@@ -1,5 +0,0 @@
-#!/bin/bash
-
-set -x
-
-qemu-system-x86_64 "VMIMGPATH" --enable-kvm -smp 2 -m 1024M -device e1000,netdev=mgmt -netdev user,id=mgmt,hostfwd=tcp::2222-:22 -vga std &
---
+++ update_vm.sh
@@ -0,0 +1,7 @@
+#!/bin/bash
+
+set -x
+
+source gen.env
+
+qemu-system-x86_64 "$vmimgpath" --enable-kvm -smp 2 -m 1024M -device e1000,netdev=mgmt -netdev user,id=mgmt,hostfwd=tcp::2222-:22 -vga std &
