IRATI/demonstrator

commit 74a0868520411636f14f19167bb17b0bc6f4d93e

scp args: slightly change codestyle to prevent possible compatibility problems

gen.py
@@ -529,12 +529,13 @@ for vmname in sorted(vms):
 
     vmid += 1
 
+
 for vmname in sorted(vms):
     vm = vms[vmname]
 
-    gen_files_conf = 'shimeth.%(name)s.*.dif da.map %(name)s.ipcm.conf ' % {'name': vmname}
-    if any(vmname in difd for difd in difs.values()):
-        gen_files_conf += 'normal.%(name)s.*.dif ' % {'name': vmname}
+    gen_files_conf = 'shimeth.%(name)s.*.dif da.map %(name)s.ipcm.conf' % {'name': vmname}
+    if any(vmname in difs[difname] for difname in difs):
+        gen_files_conf += ' normal.%(name)s.*.dif ' % {'name': vmname}
     gen_files_bin = 'enroll.py '
     overlay = ''
 

commit 78db3c23c4b60692eb21116ce96215af36c9f38b

gen.py; fix bug introduced in 74a0868
When generating gen_files_conf the line must always end with a blank space

gen.py
@@ -533,9 +533,9 @@ for vmname in sorted(vms):
 for vmname in sorted(vms):
     vm = vms[vmname]
 
-    gen_files_conf = 'shimeth.%(name)s.*.dif da.map %(name)s.ipcm.conf' % {'name': vmname}
+    gen_files_conf = 'shimeth.%(name)s.*.dif da.map %(name)s.ipcm.conf ' % {'name': vmname}
     if any(vmname in difs[difname] for difname in difs):
-        gen_files_conf += ' normal.%(name)s.*.dif ' % {'name': vmname}
+        gen_files_conf += 'normal.%(name)s.*.dif ' % {'name': vmname}
     gen_files_bin = 'enroll.py '
     overlay = ''
 